fd0cdd147c79e3fccc3d690d7a5a6d102e529e44
ImSoBored420 <91338907+ImSoBored420@users.noreply.github.com>
2024-03-21 22:28:03 -0400

    Add files via upload

---
+++ cmd loopz.py
@@ -0,0 +1,48 @@
+import os
+import time
+
+def france(): #KMD better ngl
+    print("too many options")
+    time.sleep(1)
+    print("love me 'cuz I'm getting poppin")
+    time.sleep(1)
+
+france()
+
+def freestyle(keem):
+    def keem():
+
+        print("\nboutta show the serial number of the storage device just so u know uwu \n") #serial number gag
+        time.sleep(1)
+        print("actually wait\n")
+        time.sleep(.5)
+
+        joe = input("do you want me to?: ").lower() #option pick yes/no
+        if (joe) == "yes" or (joe) == "y": #there's an error here but the code runs fine so idk why? sure, it might not be the most efficient program, and it's definitely not winning any awards, but this is just harsh, man. especially when it runs fine! so this IDE can suck it
+            time.sleep(.5)
+            print("\n ok sending it now uwu")
+            time.sleep(1)
+            os.system("tree")
+        else:
+            time.sleep(.5)
+            print("\nfair enough :'( go on ahead to the next section I guess... \n")
+            time.sleep(.5)
+    keem()
+
+    path = "c://"
+    os.chdir(path)
+#loop to make it so you can do infinite stuff until you dont wanna in cmd
+    while True:
+        ninput = input("I opened the shell, what do you want me to do?: ").lower()
+
+        os.system(ninput)
+
+        more = input("\n Anything else you need me to do in cmd?: ").lower()
+        if more != "yes" and more != "y":
+            break
+
+freestyle(keem=1) #this gets rid of an error in the code. no rhyme or reason, but if it works, it works. It's not like this is gonna go public or something
+
+print("\n\n**Jesus this program was held together by fucking duct tape**\n\n")
+print("\n\n**Shoutout to The Pharcyde for making the music I listen to while creating this**\n\n")
+print("\n\n**I should be proud I even got it to the point of functionality without getting errors out the ass**\n\n")
